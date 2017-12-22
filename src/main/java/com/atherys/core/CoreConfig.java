0e2710b3918a2fbd264430409106d70d0f54e212
Haed Hutner <mvvasilev@outlook.com>
2017-12-22 20:40:09 +0200

    Added comments to the config

--- src/main/java/com/atherys/core/CoreConfig.java
+++ src/main/java/com/atherys/core/CoreConfig.java
@@ -7,25 +7,25 @@ import java.io.IOException;
 
 public final class CoreConfig extends Config {
 
-    @Setting( value = "defaultConfig")
+    @Setting( value = "defaultConfig", comment = "Whether or not this is the default config. If this is set to true, the plugin will not start.")
     public boolean defaultConfig = true;
 
-    @Setting( value = "database.host" )
+    @Setting( value = "database.host", comment = "The host ip address of the MongoDB Database.")
     public String mongoHost = "localhost";
 
-    @Setting( value = "database.port" )
+    @Setting( value = "database.port", comment = "The port on which the MongoDB Database is running." )
     public int mongoPort = 27017;
 
-    @Setting( value = "database.database" )
+    @Setting( value = "database.database", comment = "The name of the database A'therys Core will use." )
     public String mongoDatabase = "core_Database";
 
-    @Setting( value = "database.user_database" )
+    @Setting( value = "database.user_database", comment = "The name of the user database which A'therys Core will reference for authentication." )
     public String mongoUserDB = "user_Database";
 
-    @Setting( value = "database.username" )
+    @Setting( value = "database.username", comment = "The username used for authentication." )
     public String mongoUsername = "username";
 
-    @Setting( value = "database.password" )
+    @Setting( value = "database.password", comment = "The password used for authentication." )
     public String mongoPassword = "password";
 
     public CoreConfig() throws IOException {
