2de3c3d4f44d1df2b711ef43a221b6a1bf1327c3
Haed Hutner <mvvasilev@outlook.com>
2017-12-23 13:28:30 +0200

    Changed plugin ID

--- src/main/java/com/atherys/core/AtherysCore.java
+++ src/main/java/com/atherys/core/AtherysCore.java
@@ -19,7 +19,7 @@ import static com.atherys.core.AtherysCore.*;
 
 @Plugin( id = ID, version = VERSION, name = NAME, description = DESCRIPTION)
 public class AtherysCore {
-    public static final String ID = "atherys-core";
+    public static final String ID = "atheryscore";
     public static final String NAME = "A'therys Core";
     public static final String DESCRIPTION = "The core utilities used on the A'therys Horizons server.";
     public static final String VERSION = "1.0.0a";
