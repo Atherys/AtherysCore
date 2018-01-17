75b566bd5e6f50a11c90887157950803a87f5f65
HaedHutner <mvvasilev@outlook.com>
2018-01-17 17:28:37 +0200

    Up version to 1.0.3

--- src/main/java/com/atherys/core/AtherysCore.java
+++ src/main/java/com/atherys/core/AtherysCore.java
@@ -22,7 +22,7 @@ public class AtherysCore {
     public static final String ID = "atheryscore";
     public static final String NAME = "A'therys Core";
     public static final String DESCRIPTION = "The core utilities used on the A'therys Horizons server.";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     private static AtherysCore instance;
 
