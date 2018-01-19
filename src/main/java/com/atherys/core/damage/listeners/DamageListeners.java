ce83c3ccd0d6026b0835f65ba539f36ca1c8d38b
HaedHutner <mvvasilev@outlook.com>
2018-01-19 21:20:33 +0200

    Fix maybe?

--- src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
+++ src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
@@ -2,7 +2,7 @@ package com.atherys.core.damage.listeners;
 
 import com.atherys.core.AtherysCore;
 import com.atherys.core.damage.sources.AtherysEntityMultiDamageSource;
-import org.spongepowered.api.Sponge;
+import com.atherys.core.damage.sources.AtherysEntitySingleDamageSource;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
@@ -12,9 +12,9 @@ public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
     public void onDirectMultiDamage ( DamageEntityEvent event, @First AtherysEntityMultiDamageSource source ) {
+        event.setCancelled(true);
+        if ( event.getCause().containsType(AtherysEntitySingleDamageSource.class) ) return;
         AtherysCore.getInstance().getLogger().info( "Multi Damage Source Detected" );
-        event.setCancelled( true );
-        Sponge.getCauseStackManager().popCause();
         source.getDamageDistribution().forEach( (k,v) -> event.getTargetEntity().damage( event.getBaseDamage() * v, k )  );
     }
 
