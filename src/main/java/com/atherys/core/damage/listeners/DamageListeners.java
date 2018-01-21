0329d9c6035fd17bfd0bc87e9b83ead3c53eef35
HaedHutner <mvvasilev@outlook.com>
2018-01-21 18:50:24 +0200

    Debug message added

--- src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
+++ src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
@@ -1,5 +1,6 @@
 package com.atherys.core.damage.listeners;
 
+import com.atherys.core.AtherysCore;
 import com.atherys.core.damage.sources.AtherysEntityMultiDamageSource;
 import com.atherys.core.damage.sources.AtherysEntitySingleDamageSource;
 import com.atherys.core.damage.sources.AtherysIndirectEntityDamageSource;
@@ -13,6 +14,7 @@ public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
     public void onDirectMultiDamage ( DamageEntityEvent event, @Last AtherysEntityMultiDamageSource source ) {
+        AtherysCore.getInstance().getLogger().info( source.getType().getName() + " detected." );
         if ( event.getCause().containsType( AtherysEntitySingleDamageSource.class  ) ||
              event.getCause().containsType( AtherysIndirectEntityDamageSource.class) ) return;
         event.setCancelled(true);
@@ -21,7 +23,7 @@ public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
     public void test ( DamageEntityEvent event, @Last DamageSource source ) {
-        
+
     }
 
 }
