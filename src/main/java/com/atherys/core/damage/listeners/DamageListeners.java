00d281807c7b192dd51c6b6a1a8bcad317adfb64
HaedHutner <mvvasilev@outlook.com>
2018-01-21 21:21:16 +0200

    What about this?

--- src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
+++ src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
@@ -5,12 +5,12 @@ import com.atherys.core.damage.sources.AtherysEntityMultiDamageSource;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
-import org.spongepowered.api.event.filter.cause.Last;
+import org.spongepowered.api.event.filter.cause.First;
 
 public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
-    public void onDirectMultiDamage ( DamageEntityEvent event, @Last AtherysDamageSource source ) {
+    public void onDirectMultiDamage ( DamageEntityEvent event, @First AtherysDamageSource source ) {
         if ( source instanceof AtherysEntityMultiDamageSource ) {
             event.setCancelled(true);
             ((AtherysEntityMultiDamageSource)source).getDamageDistribution().forEach((k, v) -> event.getTargetEntity().damage(event.getBaseDamage() * v, k));
