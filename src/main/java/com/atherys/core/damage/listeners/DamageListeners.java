c00bf64201e19920c5895da8b820cee2c5eb8e19
HaedHutner <mvvasilev@outlook.com>
2018-01-21 21:15:21 +0200

    Try this instead

--- src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
+++ src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
@@ -1,29 +1,20 @@
 package com.atherys.core.damage.listeners;
 
-import com.atherys.core.AtherysCore;
+import com.atherys.core.damage.sources.AtherysDamageSource;
 import com.atherys.core.damage.sources.AtherysEntityMultiDamageSource;
-import com.atherys.core.damage.sources.AtherysEntitySingleDamageSource;
-import com.atherys.core.damage.sources.AtherysIndirectEntityDamageSource;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.filter.cause.Last;
 
 public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
-    public void onDirectMultiDamage ( DamageEntityEvent event, @Last AtherysEntityMultiDamageSource source ) {
-        AtherysCore.getInstance().getLogger().info( source.getType().getName() + " detected." );
-        if ( event.getCause().containsType( AtherysEntitySingleDamageSource.class  ) ||
-             event.getCause().containsType( AtherysIndirectEntityDamageSource.class) ) return;
-        event.setCancelled(true);
-        source.getDamageDistribution().forEach( (k, v) -> event.getTargetEntity().damage( event.getBaseDamage() * v, k) );
-    }
-
-    @Listener ( order = Order.FIRST )
-    public void test ( DamageEntityEvent event, @Last DamageSource source ) {
-
+    public void onDirectMultiDamage ( DamageEntityEvent event, @Last AtherysDamageSource source ) {
+        if ( source instanceof AtherysEntityMultiDamageSource ) {
+            event.setCancelled(true);
+            ((AtherysEntityMultiDamageSource)source).getDamageDistribution().forEach((k, v) -> event.getTargetEntity().damage(event.getBaseDamage() * v, k));
+        }
     }
 
 }
