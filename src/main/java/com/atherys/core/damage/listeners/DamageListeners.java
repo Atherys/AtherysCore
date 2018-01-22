2aaad3b8321b5ce792a34edac29262922ee1dbec
HaedHutner <mvvasilev@outlook.com>
2018-01-22 22:33:40 +0200

    Lets try to crash the server again oh boy

--- src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
+++ src/main/java/com/atherys/core/damage/listeners/DamageListeners.java
@@ -1,20 +1,17 @@
 package com.atherys.core.damage.listeners;
 
-import com.atherys.core.damage.sources.AtherysDamageSource;
 import com.atherys.core.damage.sources.AtherysEntityMultiDamageSource;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
-import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.filter.cause.Root;
 
 public class DamageListeners {
 
     @Listener ( order = Order.FIRST )
-    public void onDirectMultiDamage ( DamageEntityEvent event, @First AtherysDamageSource source ) {
-        if ( source instanceof AtherysEntityMultiDamageSource ) {
-            event.setCancelled(true);
-            ((AtherysEntityMultiDamageSource)source).getDamageDistribution().forEach((k, v) -> event.getTargetEntity().damage(event.getBaseDamage() * v, k));
-        }
+    public void onDirectMultiDamage ( DamageEntityEvent event, @Root AtherysEntityMultiDamageSource source ) {
+        event.setCancelled( true );
+        source.getDamageDistribution().forEach((k, v) -> event.getTargetEntity().damage( event.getBaseDamage() * v, k) );
     }
 
 }
