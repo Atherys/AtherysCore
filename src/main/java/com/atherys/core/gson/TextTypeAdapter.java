f864d450d608985d321adf1a468c762a28b0889e
HaedHutner <mvvasilev@outlook.com>
2018-02-12 22:29:06 +0200

    Refactor TextTypeAdapter

--- src/main/java/com/atherys/core/gson/TextTypeAdapter.java
+++ src/main/java/com/atherys/core/gson/TextTypeAdapter.java
@@ -1,5 +1,6 @@
 package com.atherys.core.gson;
 
+import com.google.gson.JsonParser;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
@@ -10,18 +11,16 @@ import java.io.IOException;
 
 public class TextTypeAdapter extends TypeAdapter<Text> {
 
+    static JsonParser parser = new JsonParser();
+
     @Override
     public void write(JsonWriter out, Text value) throws IOException {
-        out.beginObject();
-            out.jsonValue( TextSerializers.JSON.serialize(value) );
-        out.endObject();
+        out.jsonValue( TextSerializers.JSON.serialize(value) );
     }
 
     @Override
     public Text read(JsonReader in) throws IOException {
-        in.beginObject();
-            String textJson = in.toString();
-        in.endObject();
+        String textJson = parser.parse(in).toString();
 
         return TextSerializers.JSON.deserialize( textJson );
     }
