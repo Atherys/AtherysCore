136d48a32efa89396df69753e791e47144d3db36
Haed Hutner <mvvasilev@outlook.com>
2018-03-25 21:53:55 +0300

    Forgot to add a primitive type to the damage source builder

--- src/main/java/com/atherys/core/damage/sources/AtherysDamageSources.java
+++ src/main/java/com/atherys/core/damage/sources/AtherysDamageSources.java
@@ -6,15 +6,15 @@ import org.spongepowered.api.entity.Entity;
 public class AtherysDamageSources {
 
     public static AtherysEntityDamageSourceBuilder melee ( AtherysDamageType type, Entity source ) {
-        return AtherysEntityDamageSource.builder().atherysType( type ).entity( source );
+        return AtherysEntityDamageSource.builder().type( type.getPrimitive() ).atherysType( type ).entity( source );
     }
 
     public static AtherysIndirectEntityDamageSourceBuilder ranged ( AtherysDamageType type, Entity source, Entity projectile ) {
-        return AtherysIndirectEntityDamageSource.builder().atherysType( type ).entity( source ).proxySource( projectile );
+        return AtherysIndirectEntityDamageSource.builder().type( type.getPrimitive() ).atherysType( type ).entity( source ).proxySource( projectile );
     }
 
     public static AtherysEntityDamageSourceBuilder magic ( AtherysDamageType type, Entity source ) {
-        return AtherysEntityDamageSource.builder().atherysType( type ).magical().entity( source );
+        return AtherysEntityDamageSource.builder().type( type.getPrimitive() ).atherysType( type ).magical().entity( source );
     }
 
 }
