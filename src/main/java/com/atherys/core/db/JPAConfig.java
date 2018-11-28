e5e7e52b7d20c6ae4f27e2858015e9c02cfbcf75
Miroslav Vasilev <miroslav.vasilev@scalefocus.com>
2018-11-28 16:25:51 +0200

    More work to be done on JPAConfig.

--- src/main/java/com/atherys/core/db/JPAConfig.java
+++ src/main/java/com/atherys/core/db/JPAConfig.java
@@ -3,6 +3,7 @@ package com.atherys.core.db;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
@@ -13,14 +14,20 @@ public final class JPAConfig {
 
     @ConfigSerializable
     public static class DatabaseConfig {
+        @Setting("database-address")
+        public String ADDRESS = "127.0.0.1";
+
+        @Setting("port")
+        public int PORT = 5432;
+
         @Setting("database-name")
-        public String NAME = "atheryscore";
+        public String NAME = "atherys";
 
         @Setting("database-user")
         public String USER = "postgre";
 
         @Setting("database-password")
-        public String PASSWORD = "";
+        public String PASSWORD = "pwd";
     }
 
     @ConfigSerializable
@@ -31,8 +38,8 @@ public final class JPAConfig {
         @Setting("hibernate-driver")
         public String DRIVER = "org.postgresql.Driver";
 
-        @Setting("hibernate-hdm2ddl-auto")
-        public String HDM2DDL_AUTO = "update";
+        @Setting("hibernate-hbm2ddl-auto")
+        public String HBM2DDL_AUTO = "update";
     }
 
     @Setting("database")
@@ -44,6 +51,8 @@ public final class JPAConfig {
     public DataSource getDataSource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
+        dataSource.setUrl(DB.ADDRESS);
+        dataSource.setPortNumber(DB.PORT);
         dataSource.setDatabaseName(DB.NAME);
         dataSource.setUser(DB.USER);
         dataSource.setPassword(DB.PASSWORD);
@@ -54,9 +63,9 @@ public final class JPAConfig {
     public Configuration getHibernateConfiguration() {
         Configuration properties = new Configuration();
 
-        properties.setProperty("hibernate.dialect", HIBERNATE.DIALECT);
-        properties.setProperty("hibernate.connection.driver_class", HIBERNATE.DRIVER);
-        properties.setProperty("hibernate.hbm2ddl.auto", HIBERNATE.HDM2DDL_AUTO);
+        properties.setProperty(Environment.DIALECT, HIBERNATE.DIALECT);
+        properties.setProperty(Environment.DRIVER, HIBERNATE.DRIVER);
+        properties.setProperty(Environment.HBM2DDL_AUTO, HIBERNATE.HBM2DDL_AUTO);
 
         return properties;
     }
