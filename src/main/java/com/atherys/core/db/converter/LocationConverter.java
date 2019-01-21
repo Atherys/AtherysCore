c98e6cfe13230260877c7fe54e867836f44b7357
Liam <liamreadsspam@gmail.com>
2019-01-21 16:45:33 -0400

    Implement LocationConverter

--- src/main/java/com/atherys/core/db/converter/LocationConverter.java
+++ src/main/java/com/atherys/core/db/converter/LocationConverter.java
@@ -1,4 +1,24 @@
 package com.atherys.core.db.converter;
 
-public class LocationConverter {
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import javax.persistence.AttributeConverter;
+
+public class LocationConverter implements AttributeConverter<Location<World>,String> {
+    @Override
+    public String convertToDatabaseColumn(Location<World> attribute) {
+        return String.format("%f %f %f %s", attribute.getX(), attribute.getY(), attribute.getZ(), attribute.getExtent().getName());
+    }
+
+    @Override
+    public Location<World> convertToEntityAttribute(String dbData) {
+        String[] parsed = dbData.split(" ");
+        return Sponge.getGame().getServer().getWorld(parsed[3]).get().getLocation(
+                Double.valueOf(parsed[0]),
+                Double.valueOf(parsed[1]),
+                Double.valueOf(parsed[2])
+        );
+    }
 }
