14eacc3c5e5fda214131aa69260f1aa544194eef
Miroslav Vasilev <mvvasilev@outlook.com>
2019-12-05 17:46:07 +0200

    Update AtherysCore.java

--- src/main/java/com/atherys/core/AtherysCore.java
+++ src/main/java/com/atherys/core/AtherysCore.java
@@ -34,7 +34,7 @@ public class AtherysCore {
     public static final String ID = "atheryscore";
     public static final String NAME = "A'therys Core";
     public static final String DESCRIPTION = "The core utilities used on the A'therys Horizons server.";
-    public static final String VERSION = "1.11.0";
+    public static final String VERSION = "%PROJECT_VERSION%";
 
     private static AtherysCore instance;
 
