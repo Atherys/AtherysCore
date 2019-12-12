467a892e41ce7b6e16cfdba0976c88fe6ae21d3a
Miroslav Vasilev <mvvasilev@outlook.com>
2019-12-12 17:43:06 +0200

    v2.0.1: Add AtherysLocation class for use in stead of Sponge Location

---
+++ src/main/java/com/atherys/core/world/AtherysLocation.java
@@ -0,0 +1,153 @@
+package com.atherys.core.world;
+
+import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3i;
+import org.apache.commons.lang3.ObjectUtils;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.UUID;
+
+/**
+ * An immutable representation of a location, which does not contain an Extent object, but the uuid of a world.
+ * AtherysLocation can only be used to represent a {@link Location} with a {@link World} extent.
+ */
+public class AtherysLocation {
+
+    private UUID worldUuid = null;
+
+    private Vector3d position = null;
+
+    private Vector3i blockPosition = null;
+
+    private Vector3i chunkPosition = null;
+
+    private Vector3i biomePosition = null;
+
+    public static AtherysLocation of(Location<World> location) {
+        return new AtherysLocation(
+                location.getExtent().getUniqueId(),
+                location.getPosition(),
+                location.getBlockPosition(),
+                location.getChunkPosition(),
+                location.getBiomePosition()
+        );
+    }
+
+    public AtherysLocation() {
+    }
+
+    private AtherysLocation(UUID worldUuid, Vector3d position, Vector3i blockPosition, Vector3i chunkPosition, Vector3i biomePosition) {
+        this.worldUuid = worldUuid;
+        this.position = position;
+        this.blockPosition = blockPosition;
+        this.chunkPosition = chunkPosition;
+        this.biomePosition = biomePosition;
+    }
+
+    public UUID getWorldUUID() {
+        return worldUuid;
+    }
+
+    public Vector3d getPosition() {
+        return position;
+    }
+
+    public Vector3i getBlockPosition() {
+        return blockPosition;
+    }
+
+    public Vector3i getChunkPosition() {
+        return chunkPosition;
+    }
+
+    public Vector3i getBiomePosition() {
+        return biomePosition;
+    }
+
+    /**
+     * Creates a copy of this AtherysLocation object, with the new worldUuid.
+     *
+     * @param worldUuid the new worldUuid
+     * @return A copy of this object with the different worldUuid
+     */
+    public AtherysLocation setWorldUuid(UUID worldUuid) {
+        if (worldUuid == null) throw new IllegalArgumentException("worldUuid cannot be null.");
+        return new AtherysLocation(
+                worldUuid,
+                position,
+                blockPosition,
+                chunkPosition,
+                biomePosition
+        );
+    }
+
+    /**
+     * Creates a copy of this AtherysLocation object, with the new position.
+     *
+     * @param position the new position
+     * @return A copy of this object with the different position
+     */
+    public AtherysLocation setPosition(Vector3d position) {
+        if (position == null) throw new IllegalArgumentException("position cannot be null.");
+        return new AtherysLocation(
+                worldUuid,
+                position,
+                blockPosition,
+                chunkPosition,
+                biomePosition
+        );
+    }
+
+    /**
+     * Creates a copy of this AtherysLocation object, with the new blockPosition.
+     *
+     * @param blockPosition the new blockPosition
+     * @return A copy of this object with the different blockPosition
+     */
+    public AtherysLocation setBlockPosition(Vector3i blockPosition) {
+        if (blockPosition == null) throw new IllegalArgumentException("blockPosition cannot be null.");
+        return new AtherysLocation(
+                worldUuid,
+                position,
+                blockPosition,
+                chunkPosition,
+                biomePosition
+        );
+    }
+
+    /**
+     * Creates a copy of this AtherysLocation object, with the new chunkPosition.
+     *
+     * @param chunkPosition the new chunkPosition
+     * @return A copy of this object with the different chunkPosition
+     */
+    public AtherysLocation setChunkPosition(Vector3i chunkPosition) {
+        if (chunkPosition == null) throw new IllegalArgumentException("chunkPosition cannot be null.");
+        return new AtherysLocation(
+                worldUuid,
+                position,
+                blockPosition,
+                chunkPosition,
+                biomePosition
+        );
+    }
+
+    /**
+     * Creates a copy of this AtherysLocation object, with the new biomePosition.
+     *
+     * @param biomePosition the new biomePosition
+     * @return A copy of this object with the different biomePosition
+     */
+    public AtherysLocation setBiomePosition(Vector3i biomePosition) {
+        if (biomePosition == null) throw new IllegalArgumentException("biomePosition cannot be null.");
+        return new AtherysLocation(
+                worldUuid,
+                position,
+                blockPosition,
+                chunkPosition,
+                biomePosition
+        );
+    }
+}
