d36a7ce24a69cbd1d10225107c2a256fd21c9027
CodeStripper <brentgerstenschlager@gmail.com>
2020-07-21 21:39:11 -0400

    Changed serializer to use Regex to deserialize and a custom formatter to serialize. It now behaves as it is supposed to.

--- src/main/java/com/atherys/core/serialize/DurationTypeSerializer.java
+++ src/main/java/com/atherys/core/serialize/DurationTypeSerializer.java
@@ -7,7 +7,6 @@ import ninja.leaping.configurate.objectmapping.serialize.TypeSerializer;
 import org.apache.commons.lang3.StringUtils;
 
 import java.time.Duration;
-import java.util.StringTokenizer;
 
 public class DurationTypeSerializer implements TypeSerializer<Duration> {
     @Override
@@ -18,33 +17,32 @@ public class DurationTypeSerializer implements TypeSerializer<Duration> {
             throw new ObjectMappingException("Cannot parse Duration: Is either null or empty string.");
         }
 
-        StringTokenizer tokenizer = new StringTokenizer(val);
-
+        String delimiter = "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)";
+        String[] tokens = val.split(delimiter);
         Duration result = Duration.ZERO;
 
-        while(tokenizer.hasMoreTokens()) {
-            String token = tokenizer.nextToken();
-
-            long amount = Long.parseLong(token.substring(0, token.length() - 1));
-            char lastChar = token.charAt(token.length() - 1);
-
-            switch (lastChar) {
-                case 'S':
-                    result = result.plusMillis(amount);
-                    break;
-                case 's':
-                    result = result.plusSeconds(amount);
-                    break;
-                case 'm':
-                    result = result.plusMinutes(amount);
-                    break;
-                case 'h':
-                    result = result.plusHours(amount);
-                    break;
-                case 'd':
-                    result = result.plusDays(amount);
-                    break;
+        for (int i = 1; i < tokens.length; i++) {
+            if (tokens[i - 1].matches("\\d+")) {
+                long amount = Long.parseLong(tokens[i - 1]);
+                switch (tokens[i]) {
+                    case "S":
+                        result = result.plusMillis(amount);
+                        break;
+                    case "s":
+                        result = result.plusSeconds(amount);
+                        break;
+                    case "m":
+                        result = result.plusMinutes(amount);
+                        break;
+                    case "h":
+                        result = result.plusHours(amount);
+                        break;
+                    case "d":
+                        result = result.plusDays(amount);
+                        break;
+                }
             }
+
         }
 
         return result;
@@ -52,6 +50,23 @@ public class DurationTypeSerializer implements TypeSerializer<Duration> {
 
     @Override
     public void serialize(TypeToken<?> type, Duration obj, ConfigurationNode value) throws ObjectMappingException {
-        value.setValue(obj.toString());
+        String result = "";
+        if (obj != null) {
+            long days = obj.toDays();
+            obj = obj.minusDays(days);
+            long hours = obj.toHours();
+            obj = obj.minusHours(hours);
+            long minutes = obj.toMinutes();
+            obj = obj.minusMinutes(minutes);
+            long seconds = obj.getSeconds();
+            obj = obj.minusSeconds(seconds);
+            long millis = obj.toMillis();
+            result = (days == 0 ? "" : days + " d") +
+                    (hours == 0 ? "" : hours + " h") +
+                    (minutes == 0 ? "" : minutes + "m") +
+                    (seconds == 0 ? "" : seconds + "s") +
+                    (seconds == 0 ? "" : millis + "M");
+        }
+        value.setValue(result);
     }
 }
