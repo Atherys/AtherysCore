616a20222b33b4acbbb3e5f72b00aaff60e85fa7
Liam <liamreadsspam@gmail.com>
2021-06-06 22:08:54 -0300

    Woops

--- src/main/java/com/atherys/core/utils/TimeUtils.java
+++ src/main/java/com/atherys/core/utils/TimeUtils.java
@@ -7,23 +7,24 @@ import java.time.Duration;
 public class TimeUtils {
     public static String durationToString(Duration duration) {
         String result = "";
-        if (obj != null) {
-            long days = obj.toDays();
-            obj = obj.minusDays(days);
-            long hours = obj.toHours();
-            obj = obj.minusHours(hours);
-            long minutes = obj.toMinutes();
-            obj = obj.minusMinutes(minutes);
-            long seconds = obj.getSeconds();
-            obj = obj.minusSeconds(seconds);
-            long millis = obj.toMillis();
+        if (duration != null) {
+            long days = duration.toDays();
+            duration = duration.minusDays(days);
+            long hours = duration.toHours();
+            duration = duration.minusHours(hours);
+            long minutes = duration.toMinutes();
+            duration = duration.minusMinutes(minutes);
+            long seconds = duration.getSeconds();
+            duration = duration.minusSeconds(seconds);
+            long millis = duration.toMillis();
             result = (days == 0 ? "" : days + "d") +
                     (hours == 0 ? "" : hours + "h") +
                     (minutes == 0 ? "" : minutes + "m") +
                     (seconds == 0 ? "" : seconds + "s") +
                     (seconds == 0 ? "" : millis + "M");
         }
-        value.setValue(result);
+
+        return result;
     }
 
     public static Duration stringToDuration(String durationString) {
