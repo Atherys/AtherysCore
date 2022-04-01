609ce0e06c008ace8e620299d1adf2e817a1342b
Liam <liamreadsspam@gmail.com>
2022-04-01 11:39:41 -0300

    Add extra removal function and return types

--- src/main/java/com/atherys/core/economy/Economy.java
+++ src/main/java/com/atherys/core/economy/Economy.java
@@ -5,6 +5,7 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.service.economy.Currency;
 import org.spongepowered.api.service.economy.account.Account;
 import org.spongepowered.api.service.economy.account.UniqueAccount;
+import org.spongepowered.api.service.economy.transaction.TransactionResult;
 import org.spongepowered.api.service.economy.transaction.TransferResult;
 
 import java.math.BigDecimal;
@@ -66,20 +67,15 @@ public final class Economy {
         return AtherysCore.getEconomyService().flatMap(econ -> econ.getOrCreateAccount(account));
     }
 
-    public static void addCurrency(UUID account, Currency currency, BigDecimal amount, Cause cause) {
-        AtherysCore.getEconomyService().ifPresent(econ -> {
-            econ.getOrCreateAccount(account).ifPresent(uniqueAccount -> {
-                uniqueAccount.deposit(currency, amount, cause);
-            });
-        });
+    public static Optional<TransactionResult> addCurrency(UUID account, Currency currency, BigDecimal amount, Cause cause) {
+        return getAccount(account).map(uniqueAccount -> uniqueAccount.deposit(currency, amount, cause));
     }
 
-    public static void removeCurrency(UUID account, Currency currency, BigDecimal amount, Cause cause) {
-        AtherysCore.getEconomyService().ifPresent(econ -> {
-            econ.getOrCreateAccount(account).ifPresent(uniqueAccount -> {
-                uniqueAccount.withdraw(currency, amount, cause);
-            });
-        });
+    public static Optional<TransactionResult> removeCurrency(UUID account, Currency currency, BigDecimal amount, Cause cause) {
+        return getAccount(account).map(uniqueAccount -> uniqueAccount.withdraw(currency, amount, cause));
     }
 
+    public static Optional<TransactionResult> removeCurrency(String account, Currency currency, BigDecimal amount, Cause cause) {
+        return getAccount(account).map(uniqueAccount -> uniqueAccount.withdraw(currency, amount, cause));
+    }
 }
